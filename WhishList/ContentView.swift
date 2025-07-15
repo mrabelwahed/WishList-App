f3a9cfbe8a95349aba79343e60afa87df74eddb7
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2025-07-15 22:05:46 +0300

    Initial Commit

---
+++ WhishList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhishList
+//
+//  Created by MahmoudRamadan on 15/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhishList/WhishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhishListApp.swift
+//  WhishList
+//
+//  Created by MahmoudRamadan on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
